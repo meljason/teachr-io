da1ed510d9219882524db665b24f1c3acfd6c2f2
DESKTOP-FHON6JU\Jason <jasoncwy@hotmail.com>
2023-05-10 18:00:23 -0400

    Fix tsx issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
-// 1. import `NextUIProvider` component
+import { AppProps } from 'next/app';
 import { NextUIProvider, createTheme } from '@nextui-org/react';
 
 const darkTheme = createTheme({ type: 'dark' });
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // 2. Use at the root of your app
     <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
